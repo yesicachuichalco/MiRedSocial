41ed3c0bc3cfea15092c0f8d3f3b179be498ddd1
YesicaChui <yesicachuic@gmail.com>
2023-03-19 11:00:22 -0500

    usando el archivo de barril para el muro y crearPublicaciones - optimizando

--- src/lib/firebasePublicaciones.js
+++ src/lib/firebasePublicaciones.js
@@ -1,16 +1,17 @@
-import FirebaseApp from '../firebaseConfig.js'
-import { getFirestore, addDoc,getDocs, collection } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js';
+// importo la configuracion de firebase del archivo de barril
+import Firebase from "../firebaseConfig.js";
 
+// pongo los nombres usuales de los objetos y funciones de firebase
+const { 
+  db, auth, addDoc, getDocs, doc, collection, deleteDoc, updateDoc, arrayRemove, arrayUnion,  
+} = Firebase;
 export const firebaseCrearPublicacion = async (texto)  =>  {
-  //conectando a la base de datos de firestore
-  const db = getFirestore(FirebaseApp);
   await addDoc(collection(db,"Publicaciones"),{publicacion:texto})
   console.log("dato insertado")
 }
 
 export const firebaseLeerPublicacion = async () => {
-  //conectando a la base de datos de firestore
-  const db = getFirestore(FirebaseApp);
+
   // con el await decimos que esperemos que termine la funcion getDocs antes de continuar
   const querySnapshot = await getDocs(collection(db,"Publicaciones"))
   let HtmlString = ""
